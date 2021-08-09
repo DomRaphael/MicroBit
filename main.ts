7ff5e1bcca4b4b6ca96602630492bad5708b91d4
Dom <66747825+DomRaphael@users.noreply.github.com>
2021-08-09 11:55:17 -0300

    Performance

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
 let ps = [0];
+let plotx = [0];
 
 let Exploded = 0
-
-let LedtoPlotx = -1
-let LedtoPloty = 0
 
 let HasTime = 1
 
@@ -29,26 +27,39 @@ function DisPlayPassword() {
     TimeAnim()
 }
 
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    basic.showNumber(0)
+    basic.pause(100)
+    basic.clearScreen()
+})
+
 function TimeAnim() {
     music.setVolume(5)
     while (HasTime) {
-        if (LedtoPloty == 5 && LedtoPlotx == 3) {
+        if (led.point(3, 4)) {
             led.plot(4,4)
             HasTime = 0
             Explode()
         } else {
-            if (LedtoPlotx == 4) {
-                LedtoPlotx = -1
-                LedtoPloty += 1
+            for (let i = 0; i < 4; i++) {
+                ps[i] = 1
+                led.plot(i,4)
+                basic.pause(1000)
             }
-            LedtoPlotx += 1
-            led.plot(LedtoPlotx, LedtoPloty)
-            music.playTone(988, music.beat(BeatFraction.Whole))
-            basic.pause(400)
         }
     }
 }
 
 function Explode() {
     music.playTone(255, music.beat(BeatFraction.Whole))
+    basic.clearScreen()
+    basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
 }
